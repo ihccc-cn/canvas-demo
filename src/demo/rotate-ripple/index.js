0ef291d27f52d3e1ae038bc58f66a7108a1f94c6
liucdev <1058807007@qq.com>
2021-06-19 17:07:12 +0800

    修改实现方式

--- src/demo/rotate-ripple/index.js
+++ src/demo/rotate-ripple/index.js
@@ -13,7 +13,7 @@ function main() {
    * 将指定数量粒子根据三角函数曲线，进行移动，并且旋转
    * 1、在圆上做正、余弦运动，求 x 和 y 坐标
    *    a、获取圆上的点
-   *    b、求以当前点为圆心的坐标系上的正、余弦曲线上的点
+   *    b、以正、余弦曲线作为偏移量来偏移圆上的点
    * 2、将正、余弦的K值设置在圆的边界上
    * 3、限制粒子在圆外运动，如果运动到圆内，将 A 值调整为原来的 1/4，运动到圆外，恢复
    * 4、根据周期性调整 A 值，每过1/4个周期修改一次A值
@@ -45,13 +45,24 @@ function main() {
     // ρ = a * sin(k * θ) + A
 
     for (let i = 0; i < ONECYCLE; i += state.count) {
-      // const { x, y } = circle.getPointInBoundary(i);
+      let { x, y } = circle.getPointInBoundary(i);
 
-      const x1 = circle.radius * Math.sin(9 * i) * Math.cos(i) * 1.2 + circle.center.x;
-      const y1 = circle.radius * Math.sin(9 * i) * Math.sin(i) * 1.2 + circle.center.y;
+      // const x1 = circle.radius * Math.sin(9 * i) * Math.cos(i) * 1.2 + circle.center.x;
+      // const y1 = circle.radius * Math.sin(9 * i) * Math.sin(i) * 1.2 + circle.center.y;
+
+      const offset = trig.sin({
+        A: 12,
+        W: 1,
+        X: ((i / state.count) * ONECYCLE) / 360,
+        D: 0,
+        K: 0,
+      });
+
+      x += offset * Math.cos(i);
+      y += offset * Math.sin(i);
 
       // console.log(x);
-      ctx.fillRect(x1, y1, 2, 2);
+      ctx.fillRect(x, y, 2, 2);
     }
   }
 
